102fda3e0f90d55dae5a3fd61a2fb005b75563ba
Mahajan <priyanka.b.mahajan@accenture.com>
2024-01-22 11:58:08 +0530

    adding API endpoint for auth token

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-const express = require('express');
-const axios = require('axios');
-const bodyParser = require('body-parser');
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
 
 const app = express();
 const PORT = 3001;
 
 app.use(bodyParser.json());
-
-app.post('/execute', async (req, res) => {
+// Enable CORS for all routes
+app.use(cors());
+app.post("/execute", async (req, res) => {
+  const {
+    clientId,
+    clientSecret,
+    language,
+    versionIndex,
+    userAnswers,
+    problems,
+  } = req.body;
+  const requestCallsBody = userAnswers?.map((ans, idx) => ({
+    clientId,
+    clientSecret,
+    language,
+    versionIndex,
+    script: ans,
+    stdin: problems[idx?.testCases],
+  }));
   try {
-    const response = await axios.post('https://api.jdoodle.com/v1/execute', req.body);
+    const promises = requestCallsBody((req) =>
+      axios.post("https://api.jdoodle.com/v1/execute", req)
+    );
+    const response = await axios.post(
+      "https://api.jdoodle.com/v1/execute",
+      req.body
+    );
+    const results = Promise.allSettled(promises);
+    console.log("results ", results);
+    // res.json(response.data);
+  } catch (error) {
+    console.error("Error calling JDoodle API:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+app.post("/auth-token", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://api.jdoodle.com/v1/auth-token",
+      req.body
+    );
     res.json(response.data);
   } catch (error) {
-    console.error('Error calling JDoodle API:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error calling JDoodle API:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
